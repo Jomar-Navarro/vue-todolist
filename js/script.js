d2ffc99ed9cd9e75ff89bf548d5f6f5d7b1757b7
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-13 14:45:57 +0100

    [add] createApp

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 console.log('Vue To Do List');
 
+const { createApp } = Vue;
+
+createApp({
+
+  data(){
+
+    return {
+
+    }
+
+  },
+
+  methods: {
+
+  },
+
+  mounted(){
+
+  },
+
+}).mount('#app');
